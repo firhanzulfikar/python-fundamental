a21f4aed41911930ef5238373ef9c74476ce32d6
SAMSUNG <midnov26@gmail.com>
2022-07-21 20:40:12 +0700

    Tugas dari dari course " Menguasai Fundamental Python untuk Remoter Worker Indonesia " di Video ke 16 Tentang Percabangan tingkat lanjut.

--- tugas 1.py
+++ tugas 1.py
@@ -1,9 +1,6 @@
-"""
-Semua Sintaksis dasar bahasa pemrograman terdiri dari
-1. Sekuensial : Langkah Berurutan
-2. Percabangan : Langkah melompat jika kondisi terpenuhi
-3. Perulangan : Mengulang langkah yang sama berkali-kali selama/sampai kodisi teroenuhi
-"""
+
+#Tugas : Buatlah Coding yang menghasilkan hasil "Budi membeli satu botol susu dan enam butir telur jika di toko ada telur"
+
 #Sekuensial
 print('Ibu berkata,"Pergi ke toko"')
 print('Budi Menjawab, "Baik, apa yang harus saya lakukan di toko?"')
